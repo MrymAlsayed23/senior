e890cf10afc2916a7bdb150150e086f9acabdcdc
MrymAlsayed23 <93190489+MrymAlsayed23@users.noreply.github.com>
2024-03-13 20:27:23 +0300

    Update Profile.php
    add the nav bar

--- Profile.php
+++ Profile.php
@@ -30,6 +30,40 @@ session_start();
     </style>
     
     <body>
+
+    <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="/senior/Images/Logo.jpg" alt="Logo" width="30" height="24">
+            </a>
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="Home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Company</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="Login.php">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </nav>
         
         <?php
         
