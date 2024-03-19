4d9cc13b14891a122ebe5f0b6bcbbf91ed203a7b
MrymAlsayed23 <93190489+MrymAlsayed23@users.noreply.github.com>
2024-03-19 19:50:38 +0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -10,6 +10,7 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
         <title>Login Page</title>
     </head>
     <style>
@@ -27,6 +28,40 @@ session_start();
         }
     </style>
     <body>
+
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="/senior/Images/Logo.jpg" alt="Logo" width="30" height="24">
+            </a>
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="Home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Company</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="Login.php">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </nav>
 
         <div class="container">
 
@@ -48,7 +83,6 @@ session_start();
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" name="remember">
                     <label for="remember" class="form-check-label">Remember me</label>
-                    <p href="#"><a href="">Reset Password?</a> </p>
                 </div>
                 
               
