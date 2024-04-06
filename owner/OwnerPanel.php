cc57664cc9985d13ad0449225e81d3b6e1eec332
20195572 <20195572@stu.uob.edu.bh>
2024-04-07 02:07:17 +0300

    null

--- owner/OwnerPanel.php
+++ owner/OwnerPanel.php
@@ -4,16 +4,118 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
+    <script src="https://kit.fontawesome.com/02448b3b92.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
+    <link rel="stylesheet" href="owner.css">
   <body>
    <!--nav --> 
-    <?php
-      include "nav.php"; 
-    ?> 
+   
+<?php 
+    //include "nav.php";
+ 
+ ?> 
+
+
+
+<div class="container">
+ <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+
+ <div class="first">
+
+ <div class="row">
+    <div class="col-6 col-md-3">
+
+    <div class="card">
+  <div class="card-body">
+  <div class="card-icon">
+  <i class="fa-solid fa-sack-dollar fa-2xl"></i>
+  </div>
+    <h5 class="card-title">$12400</h5>
+    <p class="card-text">Total Revenue</p>
+  </div>
+</div>
+    </div>
+
+
+    <div class="col-6 col-md-3">
+    <div class="card">
+  <div class="card-body">
+  <div class="card-icon">
+  <i class="fa-solid fa-boxes-packing fa-2xl"></i>
+  </div>
+  <h5 class="card-title">12334</h5>
+    <p class="card-text">Total Orders</p>
+    
+  </div>
+</div>
+    </div>
+
+
+    <div class="col-6 col-md-3">
+    <div class="card">
+  <div class="card-body">
+  <div class="card-icon">
+  <i class="fa-solid fa-check fa-2xl"></i>
+  </div>
+    <h5 class="card-title">1240</h5>
+    <p class="card-text">Completed Orders</p>
+  </div>
+</div>
+    </div>
 
     
+    <div class="col-6 col-md-3">
 
+    <div class="card">
+  <div class="card-body">
+  <div class="card-icon">
+  <i class="fa-solid fa-users fa-2xl"></i>
+  </div>
+    <h5 class="card-title">1240</h5>
+    <p class="card-text">Total Customers</p>
+    
+  </div>
+</div>
+    </div>
+  </div>
+ </div>
+
+
+ 
+
+
+
+  <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+
+  <div class="second">
+
+  <div class="row">
+    <div class="col-md-8">.col-md-8</div>
+    <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+  </div>
+
+
+  </div>
+  
+ 
+    <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+    <div class="third">
+    <div class="row">
+    <div class="col-md-8">.col-md-8</div>
+    <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+  </div>
+
+
+  
+
+    </div>
+    
+
+
+  
+</div>
+
+ 
 
 
 
--- owner/nav.php
+++ owner/nav.php
@@ -6,15 +6,13 @@
     <script src="https://kit.fontawesome.com/02448b3b92.js" crossorigin="anonymous"></script>
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="o.css">
+    <link rel="stylesheet" href="owner.css">
   </head>
   <body>
 
-
     <div class="nav">
-
-
        <aside id="sidebar">
+
         <div class="d-flex">
             <button id="togglebutton" type="button">
             <i class="fa-solid fa-bars fa-sm"></i>
@@ -23,6 +21,7 @@
                 <a href="">Title</a>
             </div>
         </div>
+        
         <ul class="sidebar-nav">
             <li class="sidebar-item">
                 <a href="" class="sidebar-link">
@@ -76,22 +75,6 @@
 
 
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     <script src= "sidebarToggle.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>     
