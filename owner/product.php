aa6452ed66afaffcddaf7e0d29029c9fe88223f3
MrymAlsayed23 <93190489+MrymAlsayed23@users.noreply.github.com>
2024-04-08 11:56:44 +0300

    Update product.php

--- owner/product.php
+++ owner/product.php
@@ -156,7 +156,9 @@
     ?>
                 <div class="alert a-cont " role="alert">
                     <div class="modal-body">
-                        <p>New Product has been inserted successfully</p>
+                        <script>
+                            alert("New Product has been inserted successfully");
+                        </script>                    
                     </div>
                 </div>
 
@@ -173,9 +175,7 @@
     ob_end_flush();
     ?>
 
-    <!-- <script>
-        alert("New Product has been inserted successfully");
-    </script> -->
+
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
