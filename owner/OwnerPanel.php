cd059d52bd63bd2d35cf972edb8673fd84076cb6
20195572 <20195572@stu.uob.edu.bh>
2024-05-08 19:21:00 +0300

    Zahraa

--- owner/OwnerPanel.php
+++ owner/OwnerPanel.php
@@ -19,10 +19,17 @@ $bid = $_GET['bid'];
     <link rel="stylesheet" href="owner.css">
   <body>
    <!--nav --> 
-   
+   <?php 
+     require('../connection.php');
+     $sql00 = "SELECT * FROM business WHERE bid='$bid'";
+     $r00 = $db->query($sql00);
+     while ($d00 = $r00->fetch(PDO::FETCH_ASSOC)){
+      extract($d00);
+     
+   ?>
    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top pb-2">
       <div class="container-fluid">
-        <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+        <a class="navbar-brand" href="#"><?php echo $d00['bname'];?></a> <?php }?>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
