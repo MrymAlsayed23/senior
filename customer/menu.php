1808559d4f6ddde775d0b5d74b353ae4ab1398c9
MrymAlsayed23 <93190489+MrymAlsayed23@users.noreply.github.com>
2024-05-11 00:14:58 +0300

    Update menu.php

--- customer/menu.php
+++ customer/menu.php
@@ -1,6 +1,6 @@
 <?php
-    session_start();
-    
+    // session_start();
+    $bid = $_GET['bid'];  
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,35 +10,141 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <title>Menu</title>
-    </head>
+</head>
+<style>
+    .card-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
 
-    <style>
-        .card-container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-        }
-
-        .card {
-            flex: 0 0 18rem;
-            margin: 10px;
-        }
+    .card {
+        flex: 0 0 18rem;
+        margin: 10px;
+    }
 </style>
 
-    <body>
+<body>
 
-        <!-- nav  -->
-        <?php include("../customer/customerNavBar.php"); ?>
+        <!-- Nav Bar  -->
+
+        <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="customerHome.php">
+                    <img src="../Images/Logo.jpg" alt="Logo" width="230" height="70">
+                </a>
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="customerHome.php">Home</a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Business
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a class="dropdown-item" href="#">
+                                            <?php
+                                                try{
+                                                    require('../connection.php');
+                                                    $sql = "SELECT bname, bid FROM business";
+                                                    $stmt = $db->query($sql); 
+                                                    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                                                    foreach ($results as $row) {
+                                                        echo "<li><a class='dropdown-item' href ='customerHome.php?bid=".$row['bid']."'>".$row['bname']."</a></li>";
+                                                    }
+                                                }
+                                                catch (PDOException $e) {
+                                                    echo "Error: " . $e->getMessage();
+                                            }
+                                            ?>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
 
 
-        <a href="cart.php">Go to cart</a>
+                            <li class="nav-item">
+                                <a class="nav-link" href="menu.php?bid<?php echo $bid; ?>">Menu</a>
+                            </li>
+
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="orderstatus.php?bid<?php echo $bid; ?>">Order Status</a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="login.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Login
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="register.php">Sign Up</a></li>
+                                    <li><a class="dropdown-item" href="profile.php">Profile</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                </nav>
+
+                <div class="header-item item-right">
+                    <?php
+                    try{
+                        require('../connection.php');
+                        $count_wishlist_items = $db->prepare("SELECT * FROM `wish_list` WHERE uid = ?");
+                        //$count_wishlist_items->execute([$user_id]);
+                        $total_wishlist_counts = $count_wishlist_items->rowCount();
+            
+                        $count_cart_items = $db->prepare("SELECT * FROM `cart` WHERE uid = ?");
+                        // $count_cart_items->execute([$uid]);
+                        $total_cart_counts = $count_cart_items->rowCount();
+                    }  
+                    catch (PDOException $e) {
+                        echo "Error: " . $e->getMessage();
+                    }
+                    ?>
+
+                            <a href="wishlist.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                                </svg>
+                            </a>
+
+                            <a href="cart.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                            </svg> 
+                            </a>
+                                                   
+                        </div>
+
+
+                    </div>
+            </nav>
+
+
+        
+
+
         <div class="card-container">
 
             
             <?php
             try {
                 require('../connection.php');
-                $sql = "SELECT * FROM products";
+                $sql = "SELECT * FROM products WHERE bid = $bid";
                 $stmt = $db->query($sql); 
                 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 foreach ($results as $row) {
@@ -56,7 +162,8 @@
                     <form method="POST">
                         <input type="number" name="pquantity" id="" min="0" max="100">
                         <input type="hidden" name="pid" id="" value='<?php echo $pid; ?>' />
-                        <button class="btn btn-primary" name="add" onclick="addToCart(<?php echo $productID; ?>)">Add to Cart</button>                    </form>
+                        <button class="btn btn-primary" name="add" onclick="addToCart(<?php echo $productID; ?>)">Add to Cart</button>                    
+                    </form>
                 </div>
             </div>
 
@@ -66,66 +173,21 @@
                 echo "Error: " . $e->getMessage();
             }
             ?>
-
-        <!-- <?php
-            // date_default_timezone_set("Asia/Bahrain");
-            // try{
-            //   require("../connection.php");
-            //   if(isset($_POST['add']))
-            //   {
-            //     extract($_POST);
-            //     if (!isset($_SESSION['username'])) {
-            //       echo"You cannot order without logging in! <br/>";
-            //       echo"<a href='login.php'>Login Now</a>";
-            //     }
-            //     else{
-            //       $data=$db->query("select pid from products where pname ='$pname'")->fetchAll();
-            //       foreach($data as $row)
-            //       {
-            //         $id=$row[0];
-            //       }
-            //       if (isset($_COOKIE['cart'])){
-            //         $previousdetails=(array)json_decode($_COOKIE['cart'],true);
-            //         foreach($previousdetails as $details){
-            //           $cookievalue[]=$details;
-            //         }
-            //       }
-            //         $details['pid']=$id;
-            //         $cookievalue[]=$details;
-            //         setcookie("cart", json_encode($cookievalue), time()+(86400*7));
-            //         echo"<br/>";
-            //         echo'Successfully added to cart';
-            //       }
-            //       $db=null;
-            //     }
-            //   }
-            // catch(PDOException $e){
-            //     echo "Failed";
-            // }
-        ?> -->
-
-
-</div>
-
+    </div>
     <script>
-        function addToCart(pid) {
-            // Send an AJAX request to add the item to the cart
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    alert("Successfully added to cart");
-                }
-            };
-            xhr.open("POST", "cart.php", true);
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xhr.send("pid=" + pid);
+        function addToCart() {
+            alert("Successfully added to cart");
         }
-        </script>
+    </script>
 
-        <!-- footer  -->
-        <?php include("../customer/footer.php"); ?>
-       
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-    </body>
+    <!-- footer  -->
+    <?php include ("../customer/footer.php"); ?>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+</body>
+
 </html>
