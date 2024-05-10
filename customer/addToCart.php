6c937e8fa6e55dd7fe2385050bc69689e952c433
MrymAlsayed23 <93190489+MrymAlsayed23@users.noreply.github.com>
2024-05-11 00:16:55 +0300

    Update addToCart.php

--- customer/addToCart.php
+++ customer/addToCart.php
@@ -1,13 +1,16 @@
 <?php
-    session_start();
-    date_default_timezone_set("Asia/Bahrain");
-   $productid = $_POST['productid'];
-   $productqty = $_POST['productqty'];
-   if ($productqty>0){
-     $productQuantity = $productqty;
-   }
-   else {
-     $productQuantity = 1;
-   }
-   $_SESSION['shoppingcart'][$productid] = $productQuantity;
-   header('location:menu.php');?>
+session_start();
+date_default_timezone_set("Asia/Bahrain");
+
+//modified
+$productid = $_POST['productid'];
+$productqty = $_POST['productqty'];
+$businessId = $_POST['businessId'];
+if ($productqty > 0) {
+  $productQuantity = $productqty;
+} else {
+  $productQuantity = 1;
+}
+$_SESSION['shoppingcart'][$productid] = $productQuantity;
+$_SESSION['shoppingcart'][$businessId] = $businessId;
+header('location:menu.php'); 
