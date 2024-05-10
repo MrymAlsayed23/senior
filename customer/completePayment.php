364bbea915612b94283d665c541b32030df55714
MrymAlsayed23 <93190489+MrymAlsayed23@users.noreply.github.com>
2024-05-11 00:19:40 +0300

    Update completePayment.php

--- customer/completePayment.php
+++ customer/completePayment.php
@@ -1,56 +1,56 @@
 <?php
-  session_start();
-  if (isset($_SESSION['uid'])){
-    if (isset($_GET['pbtn'])){
-      $type = $_GET['payType'];
-      //echo $type;
-      $total = $_GET['amount'];
-      //echo $total;
-      $card = $_GET['cardno'];
-      //echo $card;
-      $cart = $_GET['cid'];
-      //echo $cart;
-      try {
-        require('../connection.php');
-        $db->beginTransaction();
-        $sql = "INSERT INTO orders VALUES(NULL,'".$_SESSION['uid']."',NULL,$cart,$total,'Pending',NOW())";
-        $rows = $db->exec($sql);
-        if ($rows == 1){
-          $oid = $db->lastInsertId();
-          $sql = "INSERT INTO payment VALUES(NULL,'".$_SESSION['uid']."',NULL,$cart,NULL,NOW(),$total,$card)";
-          $row = $db->exec($sql);
-          $db->commit();
-        }
-        $db = null;
-      } catch (PDOException $e) {
-          //$db->rollBack();
-        die ("Error occured".$e->getMessage());
-      }
-    }
-    else if (isset($_GET['placebtn'])){
-      $type  = $_GET['payType'];
-      $total = $_GET['amount'];
-      $cart = $_GET['cid'];
-      try {
-      require('../connection.php');
+session_start();
+if (isset($_SESSION['uid'])) {
+  if (isset($_GET['pbtn'])) {
+    $type = $_GET['payType'];
+    //echo $type;
+    $total = $_GET['amount'];
+    //echo $total;
+    $card = $_GET['cardno'];
+    //echo $card;
+    $cart = $_GET['cid'];
+    //echo $cart;
+    try {
+      require ('../connection.php');
       $db->beginTransaction();
-      $sql = "INSERT INTO orders VALUES(NULL,'".$_SESSION['userid']."',NULL,$cart,$total,'Pending',NOW())";
+      $sql = "INSERT INTO orders VALUES(NULL,'" . $_SESSION['uid'] . "',NULL,$cart,$total,'Pending',NOW())";
       $rows = $db->exec($sql);
-      if ($rows == 1){
+      if ($rows == 1) {
         $oid = $db->lastInsertId();
-        $sql = "INSERT INTO payment VALUES(NULL,'".$_SESSION['uid']."',$oid,NULL,$cart,NULL,NOW(),$total,0)";
+        $sql = "INSERT INTO payment VALUES(NULL,'" . $_SESSION['uid'] . "',NULL,$cart,NULL,NOW(),$total,$card)";
         $row = $db->exec($sql);
         $db->commit();
       }
       $db = null;
     } catch (PDOException $e) {
       //$db->rollBack();
-        die ("Error occured".$e->getMessage());
+      die("Error occured" . $e->getMessage());
     }
+  } else if (isset($_GET['placebtn'])) {
+    $type = $_GET['payType'];
+    $total = $_GET['amount'];
+    $cart = $_GET['cid'];
+    try {
+      require ('../connection.php');
+      $db->beginTransaction();
+      $sql = "INSERT INTO orders VALUES(NULL,'" . $_SESSION['userid'] . "',NULL,$cart,$total,'Pending',NOW())";
+      $rows = $db->exec($sql);
+      if ($rows == 1) {
+        $oid = $db->lastInsertId();
+        $sql = "INSERT INTO payment VALUES(NULL,'" . $_SESSION['uid'] . "',$oid,NULL,$cart,NULL,NOW(),$total,0)";
+        $row = $db->exec($sql);
+        $db->commit();
+      }
+      $db = null;
+    } catch (PDOException $e) {
+      //$db->rollBack();
+      die("Error occured" . $e->getMessage());
     }
-    if(isset($_GET['cbtn'])){
-      header('location:cart.php');
-    }
-    unset($_SESSION['shoppingcart']);
+
   }
+  if (isset($_GET['cbtn'])) {
+    header('location:cart.php');
+  }
+  unset($_SESSION['shoppingcart']);
+}
 ?>
