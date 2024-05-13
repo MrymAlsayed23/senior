77e6765f773ce4589b6b30ea8f8628ebb6a4ac7b
MrymAlsayed23 <93190489+MrymAlsayed23@users.noreply.github.com>
2024-05-13 16:30:49 +0300

    Update menu.php

--- customer/menu.php
+++ customer/menu.php
@@ -9,6 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
         <title>Menu</title>
 </head>
 <style>
@@ -21,6 +22,27 @@
     .card {
         flex: 0 0 18rem;
         margin: 10px;
+    }
+    
+    .card-container .card .fa-heart{
+        position: absolute;
+        top:-1rem;
+        height: 4.5rem;
+        width: 4.5rem;
+        line-height: 4.2rem;
+        font-size: 2rem;
+        background-color: var(--white);
+        border:var(--border);
+        border-radius: .5rem;
+        text-align: center;
+        color:var(--black);
+        cursor: pointer;
+        transition: .2s linear;
+        /* right: -9rem; */
+        background-color: var(--black);
+        right: -1rem;
+        color: red;
+
     }
 </style>
 
@@ -110,9 +132,6 @@
                         //$count_wishlist_items->execute([$user_id]);
                         $total_wishlist_counts = $count_wishlist_items->rowCount();
             
-                        $count_cart_items = $db->prepare("SELECT * FROM `cart` WHERE uid = ?");
-                        // $count_cart_items->execute([$uid]);
-                        $total_cart_counts = $count_cart_items->rowCount();
                     }  
                     catch (PDOException $e) {
                         echo "Error: " . $e->getMessage();
@@ -125,11 +144,6 @@
                                 </svg>
                             </a>
 
-                            <a href="cart.php?bid=<?php echo $bid;?>">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-                                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-                            </svg> 
-                            </a>
                                                    
                         </div>
             </nav>
@@ -151,6 +165,8 @@
             
             ?>
             <div class="card" style="width: 18rem;">
+                <!-- <button class="fas fa-heart" type="submit" name="add_to_wishlist"></button> --> <!--heart icon for wishlist page-->
+                
                 <img src="<?php echo $imageSrc; ?>" class="card-img-top" alt="..."> <!-- Product Image -->
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $pname; ?></h5> <!-- Product Name -->
