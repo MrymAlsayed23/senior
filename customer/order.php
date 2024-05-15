fc71a44b18771e10d54c255f2b4363578978a1c8
MrymAlsayed23 <93190489+MrymAlsayed23@users.noreply.github.com>
2024-05-15 16:08:44 +0300

    Update order.php

--- customer/order.php
+++ customer/order.php
@@ -3,8 +3,8 @@
   $bid = $_GET['bid']; 
   ob_start(); 
   date_default_timezone_set("Asia/Bahrain");
-  if (isset($_SESSION['userid'])){
-    $uid = $_SESSION['userid'];
+  if (isset($_SESSION['uid'])){
+    $uid = $_SESSION['uid'];
   ?>
 
 <!DOCTYPE html>
@@ -237,4 +237,5 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     </body>
 </html>
-<?php } //end if statement in the beginnign of the page ?>
+<?php 
+} //end if statement in the beginnign of the page ?>
