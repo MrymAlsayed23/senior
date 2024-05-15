2c31d107f316224e0ffa3a27f4f236f30e5e5f77
MrymAlsayed23 <93190489+MrymAlsayed23@users.noreply.github.com>
2024-05-15 17:35:24 +0300

    Update orderstatus.php

--- customer/orderstatus.php
+++ customer/orderstatus.php
@@ -1,11 +1,8 @@
 <?php
 // try {
-  $bid = $_GET['bid'];
   session_start();
-  if (!isset($_SESSION["uid"])){
-    header("BusinessHome.php?bid=".$bid);
-  }
-    
+  if (isset($_SESSION["uid"])){
+    $bid = $_GET['bid'];
      
   if (isset($_SESSION['message'])) {
     echo '<script type="text/javascript">';
@@ -87,19 +84,19 @@
                                     <li>
                                         <a class="dropdown-item" href="#">
                                             <?php
-                                            //     try{
-                                            //         require('../connection.php');
-                                            //         $sql = "SELECT bname, bid FROM business";
-                                            //         $stmt = $db->query($sql); 
-                                            //         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                                                try{
+                                                    require('../connection.php');
+                                                    $sql = "SELECT bname, bid FROM business";
+                                                    $stmt = $db->query($sql); 
+                                                    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                                            //         foreach ($results as $row) {
-                                            //             echo "<li><a class='dropdown-item' href ='customerHome.php?bid=".$row['bid']."'>".$row['bname']."</a></li>";
-                                            //         }
-                                            //     }
-                                            //     catch (PDOException $e) {
-                                            //         echo "Error: " . $e->getMessage();
-                                            // }
+                                                    foreach ($results as $row) {
+                                                        echo "<li><a class='dropdown-item' href ='customerHome.php?bid=".$row['bid']."'>".$row['bname']."</a></li>";
+                                                    }
+                                                }
+                                                catch (PDOException $e) {
+                                                    echo "Error: " . $e->getMessage();
+                                            }
                                             ?>
                                         </a>
                                     </li>
@@ -152,9 +149,10 @@
       <thead>
         <tr>
           <th></th>
-          <th>Item</th>
           <th>Quantity</th>
           <th>Status</th>
+          <th>Rating</th>
+          <th>Order Summary</th>
           <th>Date/Time</th>
         </tr>
       </thead>
@@ -180,14 +178,22 @@
                  if ($c>0){
                ?>
         <tr>
-          <td>#<?php echo $c; ?></td>
-          <td> <ul>
-            <li><?php echo $rs['pname']; ?></li>
-          </ul> </td>
+          <td><?php echo $c; ?></td>
           <td> <ul>
             <li><?php echo $r['quantity'];  ?></li>
           </ul> </td>
           <td><?php echo $rows['ostatus']; ?></td>
+          
+          <td>
+            <?php
+              if ($rows['ostatus'] == 'Completed'){
+                ?>
+                <button type="button" name="add_review" id="add_review" class="btn btn-primary">Review</button>
+              <?php } ?>
+          </td>
+
+
+          <td>  <a href="order.php" class="btn btn-primary">more</a>   </td>
           <td><?php echo $rows['time']; ?></td>
         </tr>
         <tr>
@@ -206,7 +212,40 @@
     </table>
   </div>
     </div>
+  </div> <!--end div container-->
+
+    <div id="review_modal" class="modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Submit Review</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <h4 class="text-center mt-2 mb-4">
+                <i class="fas fa-star star-light submit_star mr-1" id="submit_star_1" data-rating="1"></i>
+                      <i class="fas fa-star star-light submit_star mr-1" id="submit_star_2" data-rating="2"></i>
+                      <i class="fas fa-star star-light submit_star mr-1" id="submit_star_3" data-rating="3"></i>
+                      <i class="fas fa-star star-light submit_star mr-1" id="submit_star_4" data-rating="4"></i>
+                      <i class="fas fa-star star-light submit_star mr-1" id="submit_star_5" data-rating="5"></i>
+              </h4>
+              <div class="form-group">
+                <input type="text" name="user_name" id="user_name" class="form-control" placeholder="Enter Your Name" />
+              </div>
+              <div class="form-group">
+                <textarea name="user_review" id="user_review" class="form-control" placeholder="Type Review Here"></textarea>
+              </div>
+              <div class="form-group text-center mt-4">
+                <button type="button" class="btn btn-primary" id="save_review">Submit</button>
+              </div>
+            </div>
+        </div>
+      </div>
   </div>
+
+
   <!-- footer  -->
   <?php include ("../customer/footer.php"); ?>
 
@@ -220,3 +259,224 @@
 </body>
 
 </html>
+<?php }?>
+
+<script>
+    $(document).ready(function(){
+
+        var rating_data = 0;
+
+    $('#add_review').click(function(){
+
+        $('#review_modal').modal('show');
+
+    });
+
+    $(document).on('mouseenter', '.submit_star', function(){
+
+        var rating = $(this).data('rating');
+
+        reset_background();
+
+        for(var count = 1; count <= rating; count++)
+        {
+
+            $('#submit_star_'+count).addClass('text-warning');
+
+        }
+
+    });
+
+    function reset_background()
+    {
+        for(var count = 1; count <= 5; count++)
+        {
+
+            $('#submit_star_'+count).addClass('star-light');
+
+            $('#submit_star_'+count).removeClass('text-warning');
+
+        }
+    }
+
+    $(document).on('mouseleave', '.submit_star', function(){
+
+        reset_background();
+
+        for(var count = 1; count <= rating_data; count++)
+        {
+
+            $('#submit_star_'+count).removeClass('star-light');
+
+            $('#submit_star_'+count).addClass('text-warning');
+        }
+
+    });
+
+    $(document).on('click', '.submit_star', function(){
+
+        rating_data = $(this).data('rating');
+
+    });
+
+    $('#save_review').click(function(){
+
+        var user_name = $('#user_name').val();
+
+        var user_review = $('#user_review').val();
+
+        if(user_name == '' || user_review == '')
+        {
+            alert("Please Fill Both Field");
+            return false;
+        }
+        else
+        {
+            $.ajax({
+                url:"submit_rating.php",
+                method:"POST",
+                data:{rating_data:rating_data, user_name:user_name, user_review:user_review},
+                success:function(data)
+                {
+                    $('#review_modal').modal('hide');
+
+                    load_rating_data();
+
+                    alert(data);
+                }
+            })
+        }
+    });
+    
+        $('#save_review').click(function(){
+
+        var user_name = $('#user_name').val();
+
+        var user_review = $('#user_review').val();
+
+        if(user_name == '' || user_review == ''){
+            alert("Please Fill Both Field");
+            return false;
+        }
+        else
+        {
+            $.ajax({
+                url:"submit_rating.php",
+                method:"POST",
+                data:{rating_data:rating_data, user_name:user_name, user_review:user_review},
+                success:function(data)
+                {
+                    $('#review_modal').modal('hide');
+
+                    load_rating_data();
+
+                    alert(data);
+                }
+            })
+        }
+
+        });
+
+        load_rating_data();
+
+
+    function load_rating_data(){
+        $.ajax({
+            url:"submit_rating.php",
+            method:"POST",
+            data:{action:'load_data'},
+            dataType:"JSON",
+            success:function(data){
+                $('#average_rating').text(data.average_rating);
+                $('#total_review').text(data.total_review);
+        
+                var count_star = 0;
+        
+                $('.main_star').each(function(){
+                    count_star++;
+                    if(Math.ceil(data.average_rating) >= count_star)
+                    {
+                        $(this).addClass('text-warning');
+                        $(this).addClass('star-light');
+                    }
+                });
+        
+                $('#total_five_star_review').text(data.five_star_review);
+        
+                $('#total_four_star_review').text(data.four_star_review);
+        
+                $('#total_three_star_review').text(data.three_star_review);
+        
+                $('#total_two_star_review').text(data.two_star_review);
+        
+                $('#total_one_star_review').text(data.one_star_review);
+        
+                $('#five_star_progress').css('width', (data.five_star_review/data.total_review) * 100 + '%');
+        
+                $('#four_star_progress').css('width', (data.four_star_review/data.total_review) * 100 + '%');
+        
+                $('#three_star_progress').css('width', (data.three_star_review/data.total_review) * 100 + '%');
+        
+                $('#two_star_progress').css('width', (data.two_star_review/data.total_review) * 100 + '%');
+        
+                $('#one_star_progress').css('width', (data.one_star_review/data.total_review) * 100 + '%');
+        
+                if(data.review_data.length > 0)
+                {
+                    var html = '';
+        
+                    for(var count = 0; count < data.review_data.length; count++)
+                    {
+                        html += '<div class="row mb-3">';
+        
+                        html += '<div class="col-sm-1"><div class="rounded-circle bg-danger text-white pt-2 pb-2"><h3 class="text-center">'+data.review_data[count].user_name.charAt(0)+'</h3></div></div>';
+        
+                        html += '<div class="col-sm-11">';
+        
+                        html += '<div class="card">';
+        
+                        html += '<div class="card-header"><b>'+data.review_data[count].user_name+'</b></div>';
+        
+                        html += '<div class="card-body">';
+        
+                        for(var star = 1; star <= 5; star++)
+                        {
+                            var class_name = '';
+        
+                            if(data.review_data[count].rating >= star)
+                            {
+                                class_name = 'text-warning';
+                            }
+                            else
+                            {
+                                class_name = 'star-light';
+                            }
+        
+                            html += '<i class="fas fa-star '+class_name+' mr-1"></i>';
+                        }
+        
+                        html += '<br />';
+        
+                        html += data.review_data[count].user_review;
+        
+                        html += '</div>';
+        
+                        html += '<div class="card-footer text-right">On '+data.review_data[count].datetime+'</div>';
+        
+                        html += '</div>';
+        
+                        html += '</div>';
+        
+                        html += '</div>';
+                    }
+        
+                    $('#review_content').html(html);
+                }
+        }
+    })
+    }
+    
+
+    
+    });
+</script>
